ee5f4e4f0880985f515de57a5e6de1dc16a09858
thomasrstringer <thomas.r.stringer@gmail.com>
2017-12-17 10:01:08 +1000

    james

--- data.sql
+++ data.sql
@@ -1,23 +1,8 @@
-CREATE LOGIN master WITH PASSWORD = 'tommo';
-
-GO
-
-CREATE USER tripper FOR LOGIN master
-
-GO
-
-GRANT SELECT TO tripper
-
-GO
-
-GRANT INSERT TO tripper
-
-GO
-
-GRANT UPDATE TO tripper
-
-GO
-
-GRANT DELETE TO tripper
-
-GO
+CREATE TABLE `markers` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name` VARCHAR( 60 ) NOT NULL ,
+  `address` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL ,
+  `type` VARCHAR( 30 ) NOT NULL
+) ENGINE = MYISAM ;
